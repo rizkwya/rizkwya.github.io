c140ff5c33269b1ce5416ba056e801305778e046
Rizk <87561258+rizkwya@users.noreply.github.com>
2024-01-30 00:47:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const style = `
+    background: transparent;
+    padding: 12px;
+    font-size: 2rem;
+`;
+console.log('%cRizk has made all of this.', style);
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-x');
+    navbar.classList.toggle('active');
+}
+
+let apacoba = 0;
+let lagu = document.getElementById("lagu")
+function playPause() {
+    if (apacoba == 0) {
+        apacoba = 1;
+        lagu.play();
+        lagu.loop = true;
+    }
+    else {
+        apacoba = 0;
+        lagu.pause();
+    }
+}
+
+let apabila = document.querySelectorAll('.terkadang');
+apabila.forEach(woilah => {
+    woilah.addEventListener('click', () => {
+        woilah.classList.toggle('active');
+    });
+});
