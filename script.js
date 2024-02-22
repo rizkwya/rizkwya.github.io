208f197bda74bfc3619e3ce69f3ac9086fbfd027
Rizk <87561258+rizkwya@users.noreply.github.com>
2024-02-23 06:37:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ console.log('%cRizk has made all of this.', style);
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('fa-x');
+    menuIcon.classList.toggle('fa-xmark');
     navbar.classList.toggle('active');
 }
 
