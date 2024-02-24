b2cc338dfc3bc085a46e2f6eae28924ef95f8967
Rizk <87561258+rizkwya@users.noreply.github.com>
2024-02-24 16:19:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,25 @@ apabila.forEach(woilah => {
         woilah.classList.toggle('active');
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    checkScroll();
+});
+
+window.onscroll = checkScroll;
+
+function checkScroll() {
+    const sections = document.querySelectorAll('section');
+
+    sections.forEach(sec => {
+        const top = window.scrollY;
+        const offset = sec.offsetTop - 450;
+        const height = sec.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+            sec.classList.add('show');
+        } else {
+            sec.classList.remove('show');
+        }
+    });
+}
